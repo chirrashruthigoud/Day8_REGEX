d3ecdaecaf0fc8117128c8c110db6d9224a913c2
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-05-17 13:11:40 +0530

    Add : create pattern for first name

---
+++ RegularExpressionExample/RegularExpressionExample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularExpressionExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexClass name = new RegexClass();
+            Console.WriteLine("Select one:");
+            Console.WriteLine("1.First Name \n2.Second Name \n3.PhoneNumber \n4.CheckEmail" +
+                "\n5.EmailType \n6.PassWord");
+            int num=Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    name.FirstName("Shruthi");
+                    break;
+               case 2:
+
+                    name.LastName("Guntuku");
+                    break;
+             case 3:
+                    name.PhoneNumber("+91658745960");
+                    break;
+            case 4:
+                    name.Email("abc@gmail.com");
+                    break;
+            case 5:
+                    name.EmailType("sara@2gmail.com");
+                    break;
+            case 6:
+                    name.Password("Slkal2#8521");
+                    break;
+
+                default: 
+                    Console.WriteLine("invalid");
+                    break;
+                        }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RegularExpressionExample/RegularExpressionExample/RegexClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressionExample
+{
+    public class RegexClass
+    {
+        public void FirstName(string name)
+        {
+            string pattern = "^[A-Z]{1}[a-z]{2,}$";
+            if (Regex.IsMatch(name, pattern))
+            {
+                Console.WriteLine("{0} is valid", name);
+            }
+            else
+            {
+                Console.WriteLine("{0}is invalid", name);
+            }
+        
+        }
+
+    }
+}
